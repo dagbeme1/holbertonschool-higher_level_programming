2a02a1cb889ec91e78fc5343b86958dd6bf21d7c
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2023-01-22 23:45:19 +0000

    a function that adds 2 integers, Prototype: def add_integer(a, b=98) a and b must be integers or floats, otherwise raise a TypeError exception with the message a must be an integer or b must be an integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,11 @@
 #!/bin/usr/python3
+def add_integer(a, b=98):
+    """
+    Returns a + b as int
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
