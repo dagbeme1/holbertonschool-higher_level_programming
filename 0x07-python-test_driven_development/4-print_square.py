9701efac784bb27093b451532d7956977c5cfddb
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2023-01-22 23:57:48 +0000

    a function that prints a square with the character #.

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,14 @@
 #!/bin/usr/python3
+def print_square(size):
+    """
+    Prints square with #'s given int size
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size == 0:
+        print("", end="")
+    else:
+        print("\n".join(["#" * size for rows in range(size)]))
