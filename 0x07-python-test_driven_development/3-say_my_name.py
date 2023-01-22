9fbb21394d21a73f7a51add54abad87dfaef019d
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2023-01-22 23:56:07 +0000

    a function that prints My name is <first name> <last name>

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,11 @@
 #!/bin/usr/python3
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is [full name]"
+    """
+    if isinstance(first_name, str) and isinstance(last_name, str):
+        print("My name is {:s} {:s}".format(first_name, last_name))
+    else:
+        raise TypeError("{:s} must be a string".
+                        format("first_name" if isinstance(last_name, str)
+                               else "last_name"))
