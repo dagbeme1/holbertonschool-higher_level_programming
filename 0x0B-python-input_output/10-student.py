1651cc58115d3c1a21014540688a5ca0ff69d186
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2023-01-31 21:45:32 +0000

    a class Student that defines a student by: (based on 9-student.py)

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""
+This is the Student class
+The Student class defines a student by first & last name, & age.
+This class uses the to_json method which retrieves a dictionary
+representation of a Student instance
+"""
 
 
 class Student:
-    """Represent a student."""
-
+    """Class that defines a Student"""
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
-        self.first_name = first_name
-        self.last_name = last_name
         self.age = age
+        self.last_name = last_name
+        self.first_name = first_name
 
-    def to_json(self):
-        """Get a dictionary representation of the Student."""
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """Returns dict representation of Student"""
+        if attrs:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            return self.__dict__
