faf59414d95753a6a7363d02f600f02ce89a2002
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2023-01-31 21:49:18 +0000

    a class Student that defines a student by: (based on 10-student.py)

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,44 +1,26 @@
 #!/usr/bin/python3
 """
-Module 11-student
-Contains class Student
-that initializes public instance attributes first_name, last_name, and age,
-and has public method to_json that returns dictionary representation
-of requested attributes or all if none were requested
+This is the Student class
+The Student class defines a student by first & last name, & age.
+This class uses the to_json method which retrieves a dictionary
+representation of a Student instance
 """
 
 
-class Student():
-    """
-    Public Attributes:
-        first_name
-        last_name
-        age
-    Public Methods:
-        to_json: retrieves its dictionary representation
-    """
+class Student:
+    """Class that defines a Student"""
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes student with full name and age
-        """
-        self.first_name = first_name
         self.last_name = last_name
+        self.first_name = first_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Returns dictionary description with simple data structure
-        (list, dictionary, dictionary, string)
-        for JSON serialization of an object
-        Return:
-            Only return dict of attrs given to us
-            Return entire dict if no attrs given
-        """
-        if attrs is None:
-            return self.__dict__
+        """Returns dict representation of Student"""
+        if attrs:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
         else:
-            dic = {}
-            for att in attrs:
-                if att in self.__dict__.keys():
-                    dic[att] = self.__dict__[att]
-            return dic
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of Student instance"""
+        self.__dict__.update(json)
