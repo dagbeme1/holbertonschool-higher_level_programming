c1c5e614f32c8ab8d29742499c9a9c231c81b157
Your Name <you@example.com>
2023-04-10 01:44:38 +0000

    a script that prints 3 lines, the first line: c is fun, the sexon line is python is cool and the third is Javascript is amazing

--- 0x00-javascript-warm_up/1-multi_languages.js
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
 
-const mommaJava = 'JavaScript is amazing';
-const papaC = 'C is fun';
-const brotherPython = 'Python is cool';
-
-console.log(papaC);
-console.log(brotherPython);
-console.log(mommaJava);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
