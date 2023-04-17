c300cb960013d86cbe9cceff626a1bf14ce5c652
Your Name <you@example.com>
2023-04-17 10:22:30 +0000

    a script that reads and prints the content of a file.

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/node
 
 let filename = process.argv[2];
@@ -10,6 +9,3 @@ fs.readFile(filename, 'utf8', function (err, data) {
     console.log(data);
   }
 });
-=======
-#!/usr/bin/node
->>>>>>> fc5e959e74a77b074c70b21740f492f55b0ef174
