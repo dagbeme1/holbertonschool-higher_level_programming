7607b7b32ba9495e2e8439279fa48e448c64ac47
Your Name <you@example.com>
2023-04-17 10:51:14 +0000

    a script that prints the title of a Star Wars movie where the episode number matches a given integer.

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -1,21 +1,23 @@
 #!/usr/bin/node
-const id = process.argv[2];
-const url = 'http://swapi.co/api/films/' + id;
-const request = require('request');
+// script that prints the title of a Star Wars movie where the episode number matches a given integer.
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    body = JSON.parse(body);
-    console.log(body.title);
-  } else {
-    console.log('Erorr Code:' + response.statusCode);
+// Import request module which is used to make HTTP requests
+const request = require('request');
+// Store second arg 'movie ID' into movieID variable
+const movieID = process.argv[2];
+
+// Check if movieID was provided, if not, print error message
+if (!movieID) {
+  console.error('Please provide movie ID as an argument');
+  process.exit(1);
+}
+
+// Store URL w/ movie ID in url variable
+const url = `https://swapi-api.hbtn.io/api/films/${movieID}`;
+
+// Create request of star wars API to grab movie title
+request(url, (error, response, body) => {
+  if (!error) {
+    console.log(JSON.parse(body).title);
   }
 });
-// script that prints the title of a Star Wars movie where the episode number matches a given integer.
-// Import request module which is used to make HTTP requests
-// Store second arg 'movie ID' into movieID variable
-// Check if movieID was provided, if not, print error message
-// Store URL w/ movie ID in url variable
-// Create request of star wars API to grab movie title
