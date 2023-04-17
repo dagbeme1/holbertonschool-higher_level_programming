8f07a4839966d3aca6133f87428fd1102d8db29c
Your Name <you@example.com>
2023-04-17 10:45:59 +0000

    a script that prints the title of a Star Wars movie where the episode number matches a given integer.

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+const id = process.argv[2];
+const url = 'http://swapi.co/api/films/' + id;
+const request = require('request');
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    console.log(body.title);
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
+  }
+});
+// script that prints the title of a Star Wars movie where the episode number matches a given integer.
+// Import request module which is used to make HTTP requests
+// Store second arg 'movie ID' into movieID variable
+// Check if movieID was provided, if not, print error message
+// Store URL w/ movie ID in url variable
+// Create request of star wars API to grab movie title
